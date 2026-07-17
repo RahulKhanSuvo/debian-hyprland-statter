b1da23a65f57302a7154876c1a0ab0440d559194
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-17 15:52:09 +0600

    fix: launch fzf inside kitty from keybind; remove broken TTY detection

--- hypr/.config/hypr/wallpaper-picker.sh
+++ hypr/.config/hypr/wallpaper-picker.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 dir="$HOME/Pictures/wallpapers"
-
-if [ ! -t 0 ]; then
-  exec kitty --class wallpaper-picker -e "$0"
-fi
-
 pidof hyprpaper >/dev/null || hyprpaper &>/dev/null & disown
-
-selected=$(ls "$dir" | fzf --preview='kitty icat --clear --transfer-mode=memory --stdin=no '"$dir"'/{}' --preview-window='right:50%,border-rounded' --prompt='Wallpaper > ')
+selected=$(ls "$dir" | fzf --prompt='Wallpaper > ')
 [ -z "$selected" ] && exit 0
 hyprctl hyprpaper wallpaper ",$dir/$selected"
