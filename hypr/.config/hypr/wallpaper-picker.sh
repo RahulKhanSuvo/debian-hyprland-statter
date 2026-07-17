de7632aad380fe759747baeb91dc0e5f2e2c926f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-17 15:41:36 +0600

    Fix wallpaper picker: SUPER+N, auto-start hyprpaper

--- hypr/.config/hypr/wallpaper-picker.sh
+++ hypr/.config/hypr/wallpaper-picker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 dir="$HOME/Pictures/wallpapers"
+pidof hyprpaper >/dev/null || hyprpaper &>/dev/null & disown
 selected=$(ls "$dir" | wofi --dmenu -p "Wallpaper")
 [ -z "$selected" ] && exit 0
-path="$dir/$selected"
-hyprctl hyprpaper preload "$path"
-hyprctl hyprpaper wallpaper "HDMI-A-1,$path"
+hyprctl hyprpaper wallpaper "HDMI-A-1,$dir/$selected"
