404f726b20ff0ddc0e1d30e6be6e91bcb886524f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-17 16:14:52 +0600

    cleanup: remove wallpaper picker, fzf; restore simple hyprpaper

--- hypr/.config/hypr/wallpaper-picker.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-dir="$HOME/Pictures/wallpapers"
-selected=$(ls "$dir" | fzf --prompt='Wallpaper > ')
-[ -z "$selected" ] && exit 0
-killall swaybg 2>/dev/null
-swaybg -i "$dir/$selected" -m fill &>/dev/null & disown
